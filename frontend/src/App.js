83b87056693cd2742f10c6bfb0266dd074f03df9
Andrey_Serokhvostov <sebkd@mail.ru>
2022-03-02 23:54:46 +0300

    add files react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import AuthorList from "./components/Author";
+import axios from 'axios'
 
 
 class App extends React.Component {
@@ -11,10 +13,42 @@ class App extends React.Component {
         }
     }
 
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/authors')
+            .then(response => {
+                const authors = response.data
+                this.setState(
+                    {
+                        'authors': authors
+                    }
+                )
+            }).catch(error => console.log(error))
+    }
+
+    // componentDidMount() { // данный метод вызывается для того чтобы разместить компонент на странице
+    //     const authors = [
+    //         {
+    //             'first_name': 'Федор',
+    //             'last_name': 'Достоевский',
+    //             'birthday_year': 1821
+    //         },
+    //         {
+    //             'first_name': 'Александр',
+    //             'last_name': 'Пушкин',
+    //             'birthday_year': 1799
+    //         },
+    //     ]
+    //     this.setState( // записали в state
+    //         {
+    //             authors: authors
+    //         }
+    //     )
+    // }
+
     render() {
         return (
             <div>
-                Main App
+                <AuthorList authors={this.state.authors}/>
             </div>
         )
     }
