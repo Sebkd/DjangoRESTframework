8d9a986e0ceb5a37fc476f249d0bf829e83b9370
Andrey_Serokhvostov <sebkd@mail.ru>
2022-03-08 21:33:21 +0300

    add viewsets

--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from todoapp.models import Project, ToDo
+from todoapp.serializers import ProjectModelSerializer, ToDoModelSerializer
+
+
+class ProjectModelViewSet(ModelViewSet):  # ModelViewSet реализует CRUD
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer  # с помощью какого сериализатора необходимо преобразовать в JSON
+
+
+class ToDoModelViewSet(ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoModelSerializer 
