6b3d3ebc679c3733fe2500a1f429ae90ae2cd922
Andrey_Serokhvostov <sebkd@mail.ru>
2022-04-13 00:28:25 +0300

    has error

--- todo_project/todoproject/schema.py
+++ todo_project/todoproject/schema.py
@@ -43,7 +43,20 @@ class Query(graphene.ObjectType):
     all_authors = graphene.List(AuthorType)
     all_users = graphene.List(UserType)
     author_by_id = graphene.Field(AuthorType, uuid=graphene.UUID(required=True))
+    books_by_author_name = graphene.List(BookType, name=graphene.String(required=True))
+    projects_by_author_name = graphene.List(ProjectType, authors=graphene.String(required=True))
+    authors_by_project_name = graphene.List(AuthorType, project=graphene.String(required=True))
 
+    def resolve_authors_by_project_name(self, info, project=None):
+        projects = Project.objects.filter(name__icontains=project).values('authors')
+        # authors = Author.objects.filter()
+        # return authors
+
+    def resolve_projects_by_author_name(self, info, authors=None):
+        projects = Project.objects.all()
+        if authors:
+            projects = projects.filter(authors__username__username=authors)
+        return projects
 
     def resolve_all_projects(self, info):  # graphene ищет resolve_
         return Project.objects.all()
@@ -63,7 +76,30 @@ class Query(graphene.ObjectType):
         except Author.DoesNotExist:
             return None
 
+    def resolve_books_by_author_name(self, info, name=None):
+        books = Book.objects.all()
+        if name:
+            books = books.filter(author__name=name)
+        return books
+
     # hello = graphene.String(default_value='Hi')
 
+# class AuthorMutation(graphene.Mutation):
+#     class Arguments:
+#        birthday_year = graphene.Int(required=True)
+#        id = graphene.ID()
+#
+#     author =  graphene.Field(AuthorType)
+#
+#     @classmethod
+#     def mutate(cls, root, info, birthday_year, id):
+#         author = Author.objects.get(pk=id)
+#         author.birthday_year = birthday_year
+#         author.save()
+#         return AuthorMutation(author=author)
+#
+# class Mutation(graphene.ObjectType):
+#     update_author = AuthorMutation.Field()
 
+# schema = graphene.Schema(query=Query, mutation=Mutation)
 schema = graphene.Schema(query=Query)
